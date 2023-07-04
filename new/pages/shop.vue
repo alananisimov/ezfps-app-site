c558d37beb0a78163584b630f275f090f919caae
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-04 18:10:19 +0300

    Update shop.vue

--- new/pages/shop.vue
+++ new/pages/shop.vue
@@ -126,21 +126,15 @@
   </div>
 </section>
 </template>
-<script src="https://telegram.org/js/telegram-web-app.js"></script>
-<script>
+<script src="https://telegram.org/js/telegram-web-app.js">
 let tg = window.Telegram.WebApp;
-
 tg.expand();
-
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
-
 let item = "";
-
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
-
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
@@ -151,7 +145,6 @@ btn1.addEventListener("click", function(){
 		tg.MainButton.show();
 	}
 });
-
 btn2.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
@@ -162,7 +155,6 @@ btn2.addEventListener("click", function(){
 		tg.MainButton.show();
 	}
 });
-
 btn3.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
@@ -173,25 +165,13 @@ btn3.addEventListener("click", function(){
 		tg.MainButton.show();
 	}
 });
-
-
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
-
 let usercard = document.getElementById("usercard");
-
 let p = document.createElement("p");
-
 p.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}`;
-
-
 usercard.appendChild(p); 
+</script>
 
-
-
-
-
-
-</script>    
