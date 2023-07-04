a2d194305ca4cac20a29e1c55a6db2a9fa72cdad
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-04 18:08:16 +0300

    Update shop.vue

--- new/pages/shop.vue
+++ new/pages/shop.vue
@@ -80,7 +80,7 @@
                       <span>Free updates: <span class="font-semibold">24 months</span></span>
                   </li>
               </ul>
-              <a href="#" id="btn2" class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
+              <a id="btn2" class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
           </div>
           <!-- Pricing Card -->
           <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
@@ -118,16 +118,17 @@
                       <span>Free updates: <span class="font-semibold">36 months</span></span>
                   </li>
               </ul>
-              <a href="#" id="btn3" class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
+              <a id="btn3" class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
           </div>
+	       <div class="usercard fixed" id="usercard"></div>
       </div>
-      <div class="usercard" id="usercard"></div>
-      <script src="https://telegram.org/js/telegram-web-app.js"></script>
+     
   </div>
 </section>
 </template>
+<script src="https://telegram.org/js/telegram-web-app.js"></script>
 <script>
-    let tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp;
 
 tg.expand();
 
