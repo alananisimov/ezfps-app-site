d12be8cb407199f233c24011cbf96d5a8d80c177
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-04 17:31:54 +0300

    Update app.vue

--- new/app.vue
+++ new/app.vue
@@ -1,9 +1,12 @@
 <template>
-  <NavBar/>
-  <NuxtPage/>
+  <div id="example">
 
+  
+  <NavBar v-if="new1w3"/>
+  <NuxtPage/>
+</div>
 </template>
-<script setup>
+<script setup lang="ts">
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
 
@@ -11,4 +14,20 @@ import { initFlowbite } from 'flowbite'
 onMounted(() => {
     initFlowbite();
 })
+let page = useRoute()
+let new1w3 = false
+if(page.name !== 'shop'){
+    new1w3 = true
+}
 </script>
+<style>
+.example::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.example {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+</style>
