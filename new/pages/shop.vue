1679fe37e13963077076014b0300ab38258367ca
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-04 18:38:01 +0300

    Update shop.vue

--- new/pages/shop.vue
+++ new/pages/shop.vue
@@ -119,8 +119,9 @@
                   </li>
               </ul>
               <button id="btn3" class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</button>
+		  <div class="fixed" id="usercard">123</div>
           </div>
-	       <div class="fixed" id="usercard">123</div>
+	       
       </div>
      
   </div>
