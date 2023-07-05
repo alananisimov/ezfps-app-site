28c7869704fceacf48f76d122688ccf4967c41c0
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-05 12:42:58 +0300

    Update signup.vue

--- new/pages/signup.vue
+++ new/pages/signup.vue
@@ -18,7 +18,7 @@
   </button>
 </div>
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white sm:hidden">
           <img class="w-12 h-12 mr-2" src="https://i.ibb.co/F34tqJT/logo.png" alt="logo">
             ezfps
         </a>
