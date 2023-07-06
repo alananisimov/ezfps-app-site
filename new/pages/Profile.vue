bb50ef441da69cffc4edcf1a6d22999f943dce9b
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-06 11:35:11 +0300

    Update Profile.vue

--- new/pages/Profile.vue
+++ new/pages/Profile.vue
@@ -3,10 +3,10 @@
 <div id="readProductModal" tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden flex top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
     <div class="relative p-4 w-full max-w-xl h-full md:h-auto">
         <!-- Modal content -->
-        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 my-32">
+        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 ">
             <succesmodal/>
             <errormodal/>
-            <form action="/">
+            <form action="#">
                 <!-- Modal header -->
                 <div class="flex justify-between mb-4 rounded-t sm:mb-5">
                     <div class="text-lg text-gray-900 md:text-xl dark:text-white">
@@ -30,26 +30,35 @@
                     </div>
                 </div>
                 <dl>
+                    
                     <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Role</dt>
-                    <dd v-if="user" class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400"> </dd>
-                    <dd v-if="!user" class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">Unauthorized</dd>
+                    <div class="relative mb-4">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path fill="currentColor" d="M8 1V0v1Zm4 0V0v1Zm2 4v1h1V5h-1ZM6 5H5v1h1V5Zm2-3h4V0H8v2Zm4 0a1 1 0 0 1 .707.293L14.121.879A3 3 0 0 0 12 0v2Zm.707.293A1 1 0 0 1 13 3h2a3 3 0 0 0-.879-2.121l-1.414 1.414ZM13 3v2h2V3h-2Zm1 1H6v2h8V4ZM7 5V3H5v2h2Zm0-2a1 1 0 0 1 .293-.707L5.879.879A3 3 0 0 0 5 3h2Zm.293-.707A1 1 0 0 1 8 2V0a3 3 0 0 0-2.121.879l1.414 1.414ZM2 6h16V4H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v12h2V6h-2Zm0 12v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V6H0v12h2ZM2 6V4a2 2 0 0 0-2 2h2Zm16.293 3.293C16.557 11.029 13.366 12 10 12c-3.366 0-6.557-.97-8.293-2.707L.293 10.707C2.557 12.971 6.366 14 10 14c3.634 0 7.444-1.03 9.707-3.293l-1.414-1.414ZM10 9v2a2 2 0 0 0 2-2h-2Zm0 0H8a2 2 0 0 0 2 2V9Zm0 0V7a2 2 0 0 0-2 2h2Zm0 0h2a2 2 0 0 0-2-2v2Z"/>
+  </svg>
+                    </div>
+                    <input disabled="true" type="text" id="email-address-icon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="roleValue">
+                    </div>
+
                     <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Name</dt>
                     <div class="flex">
                     <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         @
                     </span>
-                    <input name="name" v-model="user.user_metadata.name" type="text" id="website-admin" class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="elonmusk">
+                    <input v-model="username" type="text" id="username" class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="elonmusk">
                     </div>
   
                     <dd v-if="!user" class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">E-Mail: Users's email</dd>
                     
                     <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white"></dt>
                     <DifficultyModes/>
+                    <advancedsettings/>
                 </dl>
                 <div class="flex justify-between items-center mt-8">
                     
                     <div class="flex items-center space-x-3 sm:space-x-4">
-                        <button type="submit" class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                        <button @click="updateuser" type="submit" class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                             <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
                             Save
                         </button>               
@@ -78,10 +87,12 @@
 </template>
 <script setup>
 
+  let username = ref('')
   
   let roleValue = ref('')
   const supabase = useSupabaseClient()
   const { data: { user } } = await supabase.auth.getUser()
+  username = user.user_metadata.name
   let { data: user_role_data, error } = await supabase
   .from('profiles')
   .select('role')
@@ -91,6 +102,13 @@
     // получаем значение ячейки роли
     roleValue = user_role_data[0].role;
 
-  
+  const updateuser = async () => {
+    
+    
+const { data, error } = await supabase.auth.updateUser({
+  data: { name: username, full_name: username }
+})
+console.log(user.user_metadata.name)
+}
 
 </script>
