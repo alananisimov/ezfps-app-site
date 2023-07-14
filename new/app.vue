16e4813e2a2f08f86f86724a12871ab58c3089be
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-14 10:10:15 +0000

    new

--- new/app.vue
+++ new/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="loading" class="fixed left-0 top-0 h-0.5 w-full z-50 bg-green-500" />
+
   <NuxtLayout>
   <NavBar/>
   <NuxtPage/>
@@ -9,15 +9,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
-
-const nuxtApp = useNuxtApp();
-  const loading = ref(false);
-  nuxtApp.hook("page:start", () => {
-    loading.value = true;
-  });
-  nuxtApp.hook("page:finish", () => {
-    loading.value = false;
-  });
 
 // initialize components based on data attribute selectors
 onMounted(() => {
