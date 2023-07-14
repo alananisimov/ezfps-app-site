8d409d059184419f6358b7be2d7fdd2adaaa26bd
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-14 10:20:29 +0000

    new

--- new/app.vue
+++ new/app.vue
@@ -1,9 +1,9 @@
 <template>
-
-  <NuxtLoadingIndicator />
+<div class="h-screen">
+  <div v-if="loading" class="fixed left-0 top-0 h-0.5 w-full z-50 bg-green-500" />
   <NavBar/>
   <NuxtPage/>
-
+</div>
 
 </template>
 <script setup lang="ts">
