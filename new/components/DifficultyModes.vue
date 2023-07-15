8a1aa82c786eb806c8293b8ee5ce73b9275d6845
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-15 16:16:44 +0000

    new

--- new/components/DifficultyModes.vue
+++ new/components/DifficultyModes.vue
@@ -2,7 +2,7 @@
 <h3 class="my-5 text-lg font-medium text-gray-900 dark:text-white">Choose boost mode:</h3>
                     <ul class="grid w-full gap-6 grid-cols-3">
                         <li>
-                            <input name="mode" type="radio" id="easy-option" value="peaceful" class="hidden peer" required="true">
+                            <input name="mode" type="radio" id="easy-option" value="peaceful" class="hidden peer">
                             <label for="easy-option" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                           
                                 <div class="block">
                                     <img class="mb-2 w-7 h-7" src="https://i.ibb.co/jkSX1v3/low-signal.png">
