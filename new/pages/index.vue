42d73df57a846bfd72249b69bcae3bc45895a3d5
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-24 12:19:20 +0300

    sdffsd

--- new/pages/index.vue
+++ new/pages/index.vue
@@ -5,9 +5,10 @@
      <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-160" aria-hidden="true">
        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ffa500] to-[#ffae00] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
      </div>
-     <div class="mx-auto max-w-2xl py-8 sm:py-24 lg:py-14 ring-1 ring-gray-900/10 rounded-full">
+     <div class="mx-auto max-w-2xl py-8 sm:py-24 lg:py-14 rounded-xl shadow-lg flex items-center space-x-4 rounded-[60px]">
        
        <div class="text-center">
+        <img src="/logo.png" class="w-24 h-24">
          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Оптимизируй свой ПК без проблем</h1>
          <p class="mt-6 text-lg leading-8 text-gray-600">Вы устали от низкой производительности и неприятных лагов на вашем ПК? Улучшите свои вычислительные возможности с помощью нашей эксклюзивной службы подписки, предназначенной для оптимизации вашей системы и повышения ее производительности.</p>
          <div class="mt-10 flex items-center justify-center gap-x-6">
