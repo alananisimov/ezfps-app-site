659e691889561107c461b1bff8897e0ace930856
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-24 15:40:40 +0300

    sdff

--- new/pages/signup.vue
+++ new/pages/signup.vue
@@ -45,7 +45,7 @@
                   </div>
                   <button type="submit" class="w-full text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Зарегистрироваться</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Нет аккаунта? <NuxtLink to="/signup"><a class="font-medium text-primary-600 hover:underline dark:text-primary-500">Войдите в аккаунт</a></NuxtLink>
+                      Есть аккаунт? <NuxtLink to="/signin"><a class="font-medium text-primary-600 hover:underline dark:text-primary-500">Войдите в аккаунт</a></NuxtLink>
                   </p>
               </form>
           </div>
