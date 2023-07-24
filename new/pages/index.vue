c1c8f820814fa042510c8a2c1f5e3010e2a084fd
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-24 12:26:03 +0300

    sdffds

--- new/pages/index.vue
+++ new/pages/index.vue
@@ -5,14 +5,14 @@
      <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-160" aria-hidden="true">
        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ffa500] to-[#ffae00] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
      </div>
-     <div class="mx-auto inline-flex max-w-2xl py-8 sm:py-24 lg:py-14 rounded-xl shadow-lg flex items-center space-x-4 rounded-[60px]">
+     <div class="mx-auto max-w-2xl py-8 sm:py-24 lg:py-14 rounded-xl shadow-lg flex items-center space-x-4 rounded-[60px]">
        
        <div class="text-center">
         <img src="/logo.png" class="w-24 h-24">
          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Оптимизируй свой ПК без проблем</h1>
          <p class="mt-6 text-lg leading-8 text-gray-600">Вы устали от низкой производительности и неприятных лагов на вашем ПК? Улучшите свои вычислительные возможности с помощью нашей эксклюзивной службы подписки, предназначенной для оптимизации вашей системы и повышения ее производительности.</p>
          <div class="mt-10 flex items-center justify-center gap-x-6">
-           <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+           <a href="#" class="rounded-full bg-orange-400 px-5 py-5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m18.525 9l-1.1-2.4l-2.4-1.1l2.4-1.1l1.1-2.4l1.1 2.4l2.4 1.1l-2.4 1.1l-1.1 2.4Zm2 7l-.8-1.7l-1.7-.8l1.7-.8l.8-1.7l.8 1.7l1.7.8l-1.7.8l-.8 1.7Zm-13 6l-.3-2.35q-.2-.075-.387-.2t-.313-.25l-2.2.95l-2.5-4.35l1.9-1.4v-.8l-1.9-1.4l2.5-4.35l2.2.95q.125-.125.313-.25t.387-.2l.3-2.35h5l.3 2.35q.2.075.388.2t.312.25l2.2-.95l2.5 4.35l-1.9 1.4v.8l1.9 1.4l-2.5 4.35l-2.2-.95q-.125.125-.312.25t-.388.2l-.3 2.35h-5Zm2.5-5q1.25 0 2.125-.875T13.025 14q0-1.25-.875-2.125T10.025 11q-1.25 0-2.125.875T7.025 14q0 1.25.875 2.125t2.125.875Z"/></svg></a>
            <a href="#" class="text-sm font-semibold leading-6 text-gray-900" id="learn">Подробнее <span aria-hidden="true">→</span></a>
          </div>
        </div>
