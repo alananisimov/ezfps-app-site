37df47cf95ad4582247872197e416ede1df94f48
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-24 20:07:20 +0300

    sdff

--- new/pages/index.vue
+++ new/pages/index.vue
@@ -11,7 +11,7 @@
         <div class="grid grid-rows-3 grid-flow-col gap-4">
     
   
-        <svg version="1.0" id="Layer_1" class="h-10 w-10 row-span-3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+        <svg version="1.0" id="Layer_1" class="h-20 w-20 row-span-3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
 	 width="800px" height="800px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
 <g>
 	<g>
@@ -26,7 +26,7 @@
 	<path fill="#F9EBB2" d="M23.999,16c0-4.418,3.586-8,8-8c4.422,0,8,3.582,8,8v8c0,4.418-3.578,8-8,8c-4.414,0-8-3.582-8-8V16z"/>
 </g>
 </svg>
-         <h1 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-900 sm:text-6xl col-span-2">Оптимизируй свой ПК без проблем</h1>
+         <h1 class="font-bold tracking-tight text-gray-900 dark:text-gray-900 col-span-2">Оптимизируй свой ПК без проблем</h1>
          <p class="row-span-2 col-span-2">sdfff</p>
          <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="/profile" class="rounded-full hover:animate-spin bg-orange-400 px-2 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="currentColor" d="m18.525 9l-1.1-2.4l-2.4-1.1l2.4-1.1l1.1-2.4l1.1 2.4l2.4 1.1l-2.4 1.1l-1.1 2.4Zm2 7l-.8-1.7l-1.7-.8l1.7-.8l.8-1.7l.8 1.7l1.7.8l-1.7.8l-.8 1.7Zm-13 6l-.3-2.35q-.2-.075-.387-.2t-.313-.25l-2.2.95l-2.5-4.35l1.9-1.4v-.8l-1.9-1.4l2.5-4.35l2.2.95q.125-.125.313-.25t.387-.2l.3-2.35h5l.3 2.35q.2.075.388.2t.312.25l2.2-.95l2.5 4.35l-1.9 1.4v.8l1.9 1.4l-2.5 4.35l-2.2-.95q-.125.125-.312.25t-.388.2l-.3 2.35h-5Zm2.5-5q1.25 0 2.125-.875T13.025 14q0-1.25-.875-2.125T10.025 11q-1.25 0-2.125.875T7.025 14q0 1.25.875 2.125t2.125.875Z"/></svg></a>
