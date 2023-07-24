d61fb15f2e33b0a866450e9fa6be8b6be586b3a2
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-24 15:18:35 +0300

    dfgg

--- new/pages/loading.vue
+++ new/pages/loading.vue
@@ -1,3 +1,5 @@
 <template>
-    <img src="/loading.svg" class="items-center justify-center p-10 h-20 w-20">
+    <div class="items-center justify-center p-10">
+    <img src="/loading.svg" class="items-center justify-center m-10">
+    </div>
 </template>
