f782c582741b13331a33d4cef047d1ddfdd7423e
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-24 20:53:17 +0300

    sdff

--- new/pages/index.vue
+++ new/pages/index.vue
@@ -54,9 +54,7 @@
        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ffae00] to-[#ffa500] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
      </div>
    </div>
-   <div v-if="!user" class="items-center justify-center px-auto py-auto h-full w-full">
-    <img src="/loading.svg" class="m-10 mx-auto my-auto mt-32">
-    </div>
+   
    </main>
 </template>
 
@@ -64,4 +62,7 @@
   const supabase = useSupabaseClient()
 const { data: { user } } = await supabase.auth.getUser()
 
+if(!user) {
+  navigateTo("/signin")
+}
 </script>
