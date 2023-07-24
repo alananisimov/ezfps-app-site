9dc5b26f7098f829d2997c080b66a38bcd379678
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-24 11:20:45 +0300

    dfgg

--- new/components/NavBar.vue
+++ new/components/NavBar.vue
@@ -13,7 +13,7 @@
           <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
               <div class="flex-shrink-0">
-                <img class="h-8 w-8" src="https://i.ibb.co/F34tqJT/logo.png" alt="Your Company" />
+                <img class="h-7 w-7" src="https://i.ibb.co/F34tqJT/logo.png" alt="Your Company" />
               </div>
               <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
@@ -23,28 +23,10 @@
             </div>
             <div class="hidden md:block">
               <div class="ml-4 flex items-center md:ml-6">
-                <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span class="sr-only">View notifications</span>
-                  <BellIcon class="h-6 w-6" aria-hidden="true" />
-                </button>
+                
   
                 <!-- Profile dropdown -->
-                <Menu as="div" class="relative ml-3">
-                  <div>
-                    <MenuButton class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span class="sr-only">Open user menu</span>
-                      <img v-if="!user" class="h-8 w-8 rounded-full" :src="user1.imageUrl" alt="" />
-                      <img v-else class="h-8 w-8 rounded-full" :src="user.user_metadata.avatar_url" alt="" />
-                    </MenuButton>
-                  </div>
-                  <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                    <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                        <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
-                      </MenuItem>
-                    </MenuItems>
-                  </transition>
-                </Menu>
+                
               </div>
             </div>
             <div class="-mr-2 flex md:hidden">
@@ -58,8 +40,7 @@
 
 
 
-                <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-                <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+               
               </DisclosureButton>
             <button id="btn_left">
                 <svg class="w-5 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
@@ -74,43 +55,7 @@
             </div>
           </div>
         </div>
-        <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-        <DisclosurePanel class="md:hidden">
-          <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            <NuxtLink to="/">
-            <DisclosureButton :key="Home" as="a" :class="[true ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="true ? 'page' : undefined">Главная</DisclosureButton>
-            </NuxtLink>
-            <NuxtLink to="/subscribtion">
-            <DisclosureButton :key="Subscribtion" as="a" :class="[false ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="false ? 'page' : undefined">Подписки</DisclosureButton>
-            </NuxtLink>
-          </div>
-          <div class="border-t border-gray-700 pb-3 pt-4">
-            <div class="flex items-center px-5">
-              <div class="flex-shrink-0">
-                <img v-if="!user" class="h-10 w-10 rounded-full" :src="user1.imageUrl" alt="" />
-                <img v-if="user" class="h-10 w-10 rounded-full" :src="user.user_metadata.avatar_url" alt="" />
-              </div>
-              <div class="ml-3">
-                <div v-if="user" class="text-base font-medium leading-none text-white">{{ user.email }}</div>
-                <div v-if="!user" class="text-base font-medium leading-none text-white">Пожалуйста войдите</div>
-              </div>
-              
-            </div>
-            <div class="mt-3 space-y-1 px-2">
-                <NuxtLink to="/signin">
-                <DisclosureButton v-if="!user" :key="Login" as="a" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Войти</DisclosureButton>
-                </NuxtLink>
-            <NuxtLink to="/profile">
-            <DisclosureButton v-if="user" :key="Profile" as="a" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Запуск</DisclosureButton>
-        </NuxtLink>
-            
-            <DisclosureButton v-if="user" :key="Logout" @click="SignOut" as="a" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Выйти</DisclosureButton>
-           
-            </div>
-          </div>
-          
-        </DisclosurePanel>
-        </transition>
+        
       </Disclosure>
   
       
