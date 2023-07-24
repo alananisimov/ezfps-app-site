cc94b65499f963dfee834033b4bd5ea77aa0754d
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-24 15:23:47 +0300

    sdff

--- new/pages/loading.vue
+++ new/pages/loading.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="items-center justify-center px-auto py-auto h-full w-full">
-    <img src="/loading.svg" class="m-10 mx-auto my-auto">
+    <img src="/loading.svg" class="m-10 mx-auto my-auto mt-48">
     </div>
 </template>
