58e27fa31cf9dd9b47c67060cf8422b08d313d06
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-27 13:49:10 +0300

    ываа

--- new/pages/Profile.vue
+++ new/pages/Profile.vue
@@ -33,7 +33,7 @@
                     
                     
                     <h3 class="my-5 text-lg font-medium text-gray-900 dark:text-white">Выберите режим оптимизации:</h3>
-                    <ul class="grid w-full gap-6 grid-cols-3">
+                    <ul class="grid w-full gap-6 grid-cols-3 whitespace-normal">
                         <li>
                             <input name="mode" type="radio" id="easy-option" value="easy" class="hidden peer" @click="changemode('easy')">
                             <label for="easy-option" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 h-[200px]">                           
