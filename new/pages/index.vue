9905d4b3f66d65f70c3e5dfbca1a9859baa224ad
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-27 17:57:18 +0300

    sdff

--- new/pages/index.vue
+++ new/pages/index.vue
@@ -35,7 +35,7 @@
         <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{user.email}}</h5>
         <span class="text-sm text-gray-500 dark:text-gray-400">{{roleValue.value}}</span>
         <div class="flex mt-4 space-x-3 md:mt-6">
-            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Конфигурации</a>
+            <a @click="" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Конфигурации</a>
             <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Уведомления</a>
         </div>
         <div class="mt-10 flex items-center justify-center gap-x-6">
@@ -70,5 +70,9 @@ if(!user) {
 
 const start_execute = () => {
   electronAPI.start();
+}
+
+const show_config = () => {
+  electronAPI.show_config();
 }
 </script>
