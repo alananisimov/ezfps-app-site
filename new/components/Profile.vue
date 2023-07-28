ec01976fab88f31ad799dd533912f4ef18e972bc
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-07-28 14:19:34 +0300

    sdff

--- new/components/Profile.vue
+++ new/components/Profile.vue
@@ -1,6 +1,6 @@
 <!-- Modal toggle -->
 <template>
-<div id="readProductModal" tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden flex top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+<div id="readProductModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
     <div class="relative p-4 w-full max-w-xl h-full md:h-auto">
         <!-- Modal content -->
         <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 ">
