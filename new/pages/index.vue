e8ef246bb3f426ac44bfa88b07ab58285ff908d8
alananisimov <alananisimov@gmail.com>
2023-08-05 13:37:51 +0300

    sdffsd

--- new/pages/index.vue
+++ new/pages/index.vue
@@ -8,7 +8,7 @@
      <succesmodal/>
      <errormodal/>
      <Profile/>
-     <div class="w-full max-w-xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+     <div class="w-full max-w-xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-16">
     <div class="flex justify-end px-4 pt-4">
         <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
             <span class="sr-only">Open dropdown</span>
@@ -33,7 +33,7 @@
     </div>
     <div class="flex flex-col pb-10 items-center justify-center">
       
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/logo.png" alt="Bonnie image"/>
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/profile.png" alt="Bonnie image"/>
         <h5 v-if="user" class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{user.email}}</h5>
         <span class="text-sm text-gray-500 dark:text-gray-400">{{roleValue.value}}</span>
         <div class="flex mt-4 space-x-3 md:mt-6">
